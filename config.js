a35a417c58a338803d22436d2faf453d5b699193
LucasLi1337Unknown <bojun792@gmail.com>
2026-08-31 17:06:27 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBXXykf9RcwQnIUaNkLCE1a8RUR-XBIKHs",
+  authDomain: "waste-time-chat.firebaseapp.com",
+  projectId: "waste-time-chat",
+  storageBucket: "waste-time-chat.firebasestorage.app",
+  messagingSenderId: "28254011996",
+  appId: "1:28254011996:web:c47df615d70bd240239c99",
+  measurementId: "G-S9M2HZRB2B"
+};
+
+export const databaseURL =
+  "https://waste-time-chat-default-rtdb.asia-southeast1.firebasedatabase.app";
